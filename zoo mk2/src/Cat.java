a337d03217e90647e081df7faefd33cf88aa41cb
CynicalCheddar <nadroj.taylor99@gmail.com>
2019-02-20 10:26:59 +0000

    Coming back from my day out at the zoo :)

--- zoo mk2/src/Cat.java
+++ zoo mk2/src/Cat.java
@@ -1,5 +1,10 @@
 public class Cat extends Animal {
 
-  //TODO:
+    public String eat(Food food) {
+        return food.eaten(this);
+    }
+    public String eaten(Cat cat) {
+        return ("cat eats food");
+    }
 
 }
--- zoo mk2/src/Dog.java
+++ zoo mk2/src/Dog.java
@@ -1,7 +1,11 @@
 public class Dog extends Animal {
 
   //TODO:
+
   public String eat(Food food) {
       return food.eaten(this);
   }
+    public String eaten(Dog dog) {
+        return ("dog eats food");
+    }
 }
