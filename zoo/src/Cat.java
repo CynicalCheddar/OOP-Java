410549238f97f544d9b3f3f539d11a138cc7b9e8
Matt Crees <matthew.crees@ntlworld.com>
2019-02-20 10:03:15 +0000

    This is zooper

--- zoo/src/Cat.java
+++ zoo/src/Cat.java
@@ -1,5 +1,13 @@
 public class Cat extends Animal {
 
   //TODO:
+  @Override
+  public String eat(Food food) {
+      return food.eaten(this);
+  }
+
+  public String eat(Fruit fruit) {
+      return fruit.eaten(this);
+  }
 
 }
