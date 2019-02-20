320c911afbadf614740031341a7bd804374648f1
Matt Crees <matthew.crees@ntlworld.com>
2019-02-20 10:27:45 +0000

    chocolate

--- zoo/src/Cat.java
+++ zoo/src/Cat.java
@@ -1,13 +1,16 @@
 public class Cat extends Animal {
 
   //TODO:
-  @Override
   public String eat(Food food) {
       return food.eaten(this);
   }
 
   public String eat(Fruit fruit) {
       return fruit.eaten(this);
+  }
+
+  public String eat(Chocolate chocolate) {
+      return chocolate.eaten(this);
   }
 
 }
